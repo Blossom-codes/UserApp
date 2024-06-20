614ab95e44f917b56faf179b25d172f12a3b5d9f
blossom.efosa <blossom.efosa@globalaccelerex.com>
2024-06-20 09:04:24 +0100

    new modification

--- src/com/blossom/Main.java
+++ src/com/blossom/Main.java
@@ -7,9 +7,16 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) System.out.println("i = " + i);
+        Input input = new Input();
+        for (int i = 1; i <= 2; i++) {
+            System.out.println("i = " + i);
+            String name = input.inputStringDataFromTerminal("Enter your name");
+            String email = input.inputStringDataFromTerminal("Enter your email");
+            int pin = input.inputIntDataFromTerminal("Set a pin");
+            Register signup = new Register(i, name,email,pin);
+            System.out.println(signup.name() +", Registration was Successful");
+            System.out.println();
 
-
-
+        }
     }
 }
